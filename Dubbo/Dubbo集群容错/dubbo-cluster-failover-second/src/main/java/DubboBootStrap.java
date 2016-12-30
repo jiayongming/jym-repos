7ddedb08cd8ccd158bfeb5d4bfb83c24eef12f0e
jiayongming <jiayongming@msn.com>
2016-12-30 12:49:50 +0800

    add dubbo failureOver

---
+++ Dubbo/Dubbo集群容错/dubbo-cluster-failover-second/src/main/java/DubboBootStrap.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@Log4j
+public class DubboBootStrap {
+	public static void main(String[] args) throws IOException {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("/config/applicationContext.xml");
+		
+		log.info("服务启动B.....");
+		System.in.read();
+		
+		
+	}
+}
